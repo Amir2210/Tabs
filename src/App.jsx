55954afb77eae72dc91ff81e83b19d327fc6a2c0
Amir Yankolovich <amiryankolovich2000@gmail.com>
2024-02-12 16:45:53 +0200

    Add onChangeCompany function that add dynamic class to the active button

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,25 @@ const url = 'https://course-api.com/react-tabs-project'
 
 const App = () => {
   const [jobs, setJobs] = useState(null)
+  const [company, setCompany] = useState('TOMMY')
+
   useEffect(() => {
     fetchData()
   }, [])
+
   async function fetchData() {
     const data = await fetch(url)
     const jobs = await data.json()
     setJobs(jobs)
   }
+
+  function onChangeCompany(company) {
+    setCompany(company)
+  }
   if (!jobs) return <div>Loading...</div>
   return (
     <section className='jobs-center'>
-      <JobCompanies jobs={jobs} />
+      <JobCompanies jobs={jobs} company={company} onChangeCompany={onChangeCompany} />
     </section>
   )
 }
--- src/cmps/JobCompanies.jsx
+++ src/cmps/JobCompanies.jsx
@@ -1,7 +1,7 @@
-export function JobCompanies({ jobs }) {
+export function JobCompanies({ jobs, company, onChangeCompany }) {
   return (
     <div className="btn-container">
-      {jobs.map(job => <button className='job-btn' key={job.id}>{job.company}</button>)}
+      {jobs.map(job => <button onClick={(event) => onChangeCompany(event.target.innerText)} className={company === job.company ? 'job-btn active-btn' : 'job-btn'} key={job.id}>{job.company}</button>)}
     </div>
   )
 }
