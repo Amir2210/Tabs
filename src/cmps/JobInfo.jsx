f33915e206ffe867d9bfb4b53d178d9915cb2880
Amir Yankolovich <amiryankolovich2000@gmail.com>
2024-02-12 17:10:25 +0200

    Create JobInfo cmp that render all the info about the selected company

--- src/cmps/JobInfo.jsx
+++ src/cmps/JobInfo.jsx
@@ -1,9 +1,21 @@
+import { FaAngleDoubleRight } from 'react-icons/fa';
 export function JobInfo({ jobs, company }) {
   const filteredJob = jobs.filter(job => job.company === company)
-  console.log(filteredJob)
   return (
-    <article className='job-info'>
-
-    </article>
+    <>
+      {filteredJob.map(job =>
+        <article key={job.id} className='job-info'>
+          <h3>{job.title}</h3>
+          <span className="job-company">{job.company}</span>
+          <p className="job-date">{job.dates}</p>
+          <div>
+            {job.duties.map((j, index) => <div className='job-desc' key={index}>
+              <FaAngleDoubleRight className='job-icon' />
+              <p>{j}</p>
+            </div>)}
+          </div>
+        </article>
+      )}
+    </>
   )
 }
